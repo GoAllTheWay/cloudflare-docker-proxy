028c98988e0a4a67dfba4cdf16ed92f2cc190d25
GoAllTheWay <490210245@qq.com>
2024-11-08 16:42:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + huadaocredit.com]: dockerHub,
+  ["quay." + huadaocredit.com]: "https://quay.io",
+  ["gcr." + huadaocredit.com]: "https://gcr.io",
+  ["k8s-gcr." + huadaocredit.com]: "https://k8s.gcr.io",
+  ["k8s." + huadaocredit.com]: "https://registry.k8s.io",
+  ["ghcr." + huadaocredit.com]: "https://ghcr.io",
+  ["cloudsmith." + huadaocredit.com]: "https://docker.cloudsmith.io",
+  ["ecr." + huadaocredit.com]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + huadaocredit.com]: dockerHub,
 };
 
 function routeByHosts(host) {
